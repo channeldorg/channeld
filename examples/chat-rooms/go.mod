c50574582dc08fcd734b476c1c004fe9d293f826
indiest <indiestian@gmail.com>
2021-10-16 19:03:20 +0800

    Added the chat room example in JavaScript(browser)

---
+++ examples/chat-rooms/go.mod
@@ -0,0 +1,7 @@
+module channeld.clewcat.com/examples/chat
+
+go 1.16
+
+require channeld.clewcat.com/channeld v0.0.0-00010101000000-000000000000
+
+replace channeld.clewcat.com/channeld => ../../../channeld
---
+++ examples/chat-rooms/server.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"channeld.clewcat.com/channeld/pkg/channeld"
+	"channeld.clewcat.com/channeld/proto"
+)
+
+var webAddr = flag.String("web", "localhost:8080", "http service address")
+var wsAddr = flag.String("ws", "localhost:12108", "websocket service address")
+
+func handleChanneldProto(w http.ResponseWriter, r *http.Request) {
+	bytes, err := os.ReadFile("../../proto/channeld.proto")
+	if err != nil {
+		log.Panic(err)
+	}
+	w.Write(bytes)
+}
+
+func handleChatProto(w http.ResponseWriter, r *http.Request) {
+	bytes, err := os.ReadFile("../../proto/example_chat_rooms.proto")
+	if err != nil {
+		log.Panic(err)
+	}
+	w.Write(bytes)
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	homeTemplate, err := template.ParseFiles("./main.html")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	homeTemplate.Execute(w, *wsAddr)
+}
+
+func main() {
+
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/proto", handleChanneldProto)
+	http.HandleFunc("/proto/chat", handleChatProto)
+
+	channeld.InitConnections(1024, "../../config/example_chat_client_fsm.json", "../../config/example_chat_client_fsm.json")
+	channeld.InitChannels()
+	channeld.GetChannel(channeld.GlobalChannelId).InitData(
+		&proto.ChatChannelData{ChatMessages: []*proto.ChatMessage{ //make([]*proto.ChatMessage, 0)},
+			{Sender: "System", SendTime: time.Now().Unix(), Content: "Welcome!"},
+		}},
+		&channeld.DataMergeOptions{ListSizeLimit: 100},
+	)
+	//channeld.SetWebSocketTrustedOrigins(["localhost"])
+	go channeld.StartListening(channeld.CLIENT, "ws", *wsAddr)
+
+	log.Fatal(http.ListenAndServe(*webAddr, nil))
+}
