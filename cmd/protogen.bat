d14910facd90b87d168b3527ff80c5b764e5e0fe
indiest <indiestian@gmail.com>
2022-08-24 17:09:13 +0800

    -Updated the package import path to make things work in Unreal C++

--- cmd/protogen.bat
+++ cmd/protogen.bat
@@ -14,7 +14,7 @@ cd "%CHANNELD_PATH%\examples\channeld-ue-chat\chatpb"
 protoc --go_out=. --go_opt=paths=source_relative -I . *.proto
 
 cd "%CHANNELD_PATH%\examples\channeld-ue-tps\tpspb"
-protoc --go_out=. --go_opt=paths=source_relative -I . -I "%CHANNELD_PATH%\pkg" *.proto
+protoc --go_out=. --go_opt=paths=source_relative -I . -I "%CHANNELD_PATH%\pkg\unrealpb" *.proto
 
 cd "%CHANNELD_PATH%\examples\unity-mirror-tanks\tankspb"
 protoc --go_out=. --go_opt=paths=source_relative -I . -I "%CHANNELD_PATH%" *.proto
