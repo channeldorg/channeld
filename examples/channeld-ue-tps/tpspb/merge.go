21570d5a29b8b4dbc00aefa96273dab3341fcef8
indiest <indiestian@gmail.com>
2022-09-16 15:41:16 +0800

    - Added 'optional' to most fields in the UE proto - Temporarily replaced the manual merge with reflection-based merge for the UE demo;

--- examples/channeld-ue-tps/tpspb/merge.go
+++ examples/channeld-ue-tps/tpspb/merge.go
@@ -1,46 +1,78 @@
 package tpspb
 
 import (
-	"errors"
-
 	"channeld.clewcat.com/channeld/pkg/channeldpb"
 	"channeld.clewcat.com/channeld/pkg/common"
-	"channeld.clewcat.com/channeld/pkg/unrealpb"
 	"google.golang.org/protobuf/proto"
 )
 
 // Implement [channeld.MergeableChannelData]
 func (dst *TestRepChannelData) Merge(src proto.Message, options *channeldpb.ChannelDataMergeOptions, spatialNotifier common.SpatialInfoChangedNotifier) error {
-	srcMsg, ok := src.(*TestRepChannelData)
-	if !ok {
-		return errors.New("src is not a TestRepChannelData")
-	}
+	// FIXME: manual copy properties instead of using reflection
+	proto.Merge(dst, src)
 
-	if dst.SceneComponentStates == nil {
-		dst.SceneComponentStates = make(map[uint32]*unrealpb.SceneComponentState)
-	}
-
-	for k, v := range srcMsg.SceneComponentStates {
-		if v.Removed {
-			delete(dst.SceneComponentStates, k)
-			continue
+	/*
+		srcMsg, ok := src.(*TestRepChannelData)
+		if !ok {
+			return errors.New("src is not a TestRepChannelData")
 		}
 
-		trans, exists := dst.SceneComponentStates[k]
-		if exists {
-			if v.RelativeLocation != nil {
-				trans.RelativeLocation = v.RelativeLocation
-			}
-			if v.RelativeRotation != nil {
-				trans.RelativeRotation = v.RelativeRotation
-			}
-			if v.RelativeScale != nil {
-				trans.RelativeScale = v.RelativeScale
-			}
-		} else {
-			dst.SceneComponentStates[k] = v
+		if dst.SceneComponentStates == nil {
+			dst.SceneComponentStates = make(map[uint32]*unrealpb.SceneComponentState)
 		}
-	}
+
+		for k, v := range srcMsg.SceneComponentStates {
+			if v.Removed {
+				delete(dst.SceneComponentStates, k)
+				continue
+			}
+
+			trans, exists := dst.SceneComponentStates[k]
+			if exists {
+				if v.RelativeLocation != nil {
+					trans.RelativeLocation = v.RelativeLocation
+				}
+				if v.RelativeRotation != nil {
+					trans.RelativeRotation = v.RelativeRotation
+				}
+				if v.RelativeScale != nil {
+					trans.RelativeScale = v.RelativeScale
+				}
+			} else {
+				dst.SceneComponentStates[k] = v
+			}
+		}
+
+		for k, v := range srcMsg.CharacterStates {
+			if v.Removed {
+				delete(dst.CharacterStates, k)
+				continue
+			}
+
+			char, exists := dst.CharacterStates[k]
+			if exists {
+				if v.RootMotion != nil {
+					if char.RootMotion == nil {
+						char.RootMotion = v.RootMotion
+					} else {
+						// FIXME: manual copy properties instead of using reflection
+						proto.Merge(char.RootMotion, v.RootMotion)
+					}
+				}
+				if v.BasedMovement != nil {
+					if char.BasedMovement == nil {
+						char.BasedMovement = v.BasedMovement
+					} else {
+						proto.Merge(char.BasedMovement, v.BasedMovement)
+					}
+				}
+				// if v.HasServerLastTransformUpdateTimeStamp() {
+				if v.ProtoReflect().Has(v.ProtoReflect().Descriptor().Fields().ByNumber(4)) {
+					char.ServerLastTransformUpdateTimeStamp = v.ServerLastTransformUpdateTimeStamp
+				}
+			}
+		}
+	*/
 
 	return nil
 }
