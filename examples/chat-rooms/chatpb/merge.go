bde89bdcf3938f853a103b7e301231ddfa18242f
Augkit <aug01000001@gmail.com>
2022-10-19 17:43:52 +0800

    Add TimeSpanLimit

--- examples/chat-rooms/chatpb/merge.go
+++ examples/chat-rooms/chatpb/merge.go
@@ -2,11 +2,15 @@ package chatpb
 
 import (
 	"errors"
+	"time"
 
 	"channeld.clewcat.com/channeld/pkg/channeldpb"
 	"channeld.clewcat.com/channeld/pkg/common"
 	"google.golang.org/protobuf/proto"
 )
+
+// Extend the merged list when the length of the merged list greater then options.ListSizeLimit
+var TimeSpanLimit time.Duration = time.Minute * 1
 
 func (dst *ChatChannelData) Merge(src proto.Message, options *channeldpb.ChannelDataMergeOptions, spatialNotifier common.SpatialInfoChangedNotifier) error {
 	srcMsg, ok := src.(*ChatChannelData)
@@ -21,15 +25,24 @@ func (dst *ChatChannelData) Merge(src proto.Message, options *channeldpb.Channel
 		dst.ChatMessages = append(dst.ChatMessages, srcMsg.ChatMessages...)
 	}
 
-	if options.ListSizeLimit > 0 {
+	lsl := int(options.ListSizeLimit)
+	n := len(dst.ChatMessages)
+	if lsl > 0 && n > lsl {
 		if options.TruncateTop {
-			start := len(dst.ChatMessages) - int(options.ListSizeLimit)
-			if start < 0 {
+			start := n - lsl
+			if TimeSpanLimit > 0 {
+				availableTime := time.Now().Add(-TimeSpanLimit)
+				for ; start > 0; start-- {
+					if time.UnixMilli(dst.ChatMessages[start-1].SendTime).Before(availableTime) {
+						break
+					}
+				}
+			} else if start < 0 {
 				start = 0
 			}
 			dst.ChatMessages = dst.ChatMessages[start:]
 		} else {
-			dst.ChatMessages = dst.ChatMessages[:options.ListSizeLimit]
+			dst.ChatMessages = dst.ChatMessages[:lsl]
 		}
 	}
 
