02610be6ff7f7b528db6aa37df6b8b90e6ee2309
indiest <indiestian@gmail.com>
2023-04-10 13:54:29 +0800

    - Implemented cascading and removing for the entity group; - AddToGroup no longer needs junctionEntityId; - Added unit tests;

--- pkg/channeld/channel.go
+++ pkg/channeld/channel.go
@@ -169,7 +169,7 @@ func createChannelWithId(channelId common.ChannelId, t channeldpb.ChannelType, o
 
 	if ch.channelType == channeldpb.ChannelType_ENTITY {
 		ch.spatialNotifier = spatialController
-		ch.entityController = &EntityChannelGroupController{}
+		ch.entityController = &FlatEntityGroupController{entityId: EntityId(channelId)}
 	}
 
 	if ch.HasOwner() {
--- pkg/channeld/entity.go
+++ pkg/channeld/entity.go
@@ -1,6 +1,7 @@
 package channeld
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/metaworking/channeld/pkg/channeldpb"
@@ -25,37 +26,64 @@ func newEntityGroup() *EntityGroup {
 	}
 }
 
+func (g *EntityGroup) Add(groupToAdd *EntityGroup) {
+	if groupToAdd == nil {
+		return
+	}
+
+	groupToAdd.entityIds.Range(func(key EntityId, value interface{}) bool {
+		g.entityIds.Store(key, value)
+		return true
+	})
+}
+
 type EntityGroupController interface {
-	SetGroup(t channeldpb.EntityGroupType, group *EntityGroup)
-	AddToGroup(t channeldpb.EntityGroupType, junction EntityId, entitiesToAdd []EntityId) error
+	cascadeGroup(t channeldpb.EntityGroupType, group *EntityGroup)
+	AddToGroup(t channeldpb.EntityGroupType, entitiesToAdd []EntityId) error
 	RemoveFromGroup(t channeldpb.EntityGroupType, entitiesToRemove []EntityId) error
 	GetHandoverEntities() []EntityId
 }
 
-type EntityChannelGroupController struct {
+// FlatEntityGroupController is a simple implementation of EntityGroupController which has
+// only one layer of handover and lock group. Adding an entities in a group to another group
+// will overwrite the previous group and the overwrite is not revertible.
+type FlatEntityGroupController struct {
+	entityId      EntityId
 	rwLock        sync.RWMutex
 	handoverGroup *EntityGroup
 	lockGroup     *EntityGroup
 }
 
-func (ctl *EntityChannelGroupController) SetGroup(t channeldpb.EntityGroupType, group *EntityGroup) {
+func (ctl *FlatEntityGroupController) cascadeGroup(t channeldpb.EntityGroupType, group *EntityGroup) {
+	// Current entity is already locked, won't cascade.
+	if ctl.lockGroup != nil && ctl.lockGroup.entityIds.Size() > 0 {
+		return
+	}
+
 	ctl.rwLock.Lock()
 	defer ctl.rwLock.Unlock()
 
 	if t == channeldpb.EntityGroupType_HANDOVER {
+		// Add the entities in current group to the new group
+		group.Add(ctl.handoverGroup)
+
+		// Set current group to the new group
 		ctl.handoverGroup = group
 	} else if t == channeldpb.EntityGroupType_LOCK {
+		// LOCK has higher priority than HANDOVER, so the cascade brings the entities in the handover group into the lock group
+		group.Add(ctl.handoverGroup)
+		group.Add(ctl.lockGroup)
+
+		// Set current group to the new group
 		ctl.lockGroup = group
 	}
 }
 
-func (ctl *EntityChannelGroupController) AddToGroup(t channeldpb.EntityGroupType, junction EntityId, entitiesToAdd []EntityId) error {
+func (ctl *FlatEntityGroupController) AddToGroup(t channeldpb.EntityGroupType, entitiesToAdd []EntityId) error {
 	if t == channeldpb.EntityGroupType_HANDOVER {
 		if ctl.handoverGroup == nil {
 			ctl.handoverGroup = newEntityGroup()
 		}
-
-		ctl.handoverGroup.entityIds.Store(junction, nil)
 
 		for _, entityId := range entitiesToAdd {
 			ctl.handoverGroup.entityIds.Store(entityId, nil)
@@ -70,31 +98,93 @@ func (ctl *EntityChannelGroupController) AddToGroup(t channeldpb.EntityGroupType
 				continue
 			}
 
-			// All entity channels of the same group use the same handover group
-			ch.entityController.SetGroup(t, ctl.handoverGroup)
+			// All entity channels of the same group share the same handover group instance
+			ch.entityController.cascadeGroup(t, ctl.handoverGroup)
+		}
+	} else if t == channeldpb.EntityGroupType_LOCK {
+		if ctl.lockGroup == nil {
+			ctl.lockGroup = newEntityGroup()
+		}
+
+		for _, entityId := range entitiesToAdd {
+			ctl.lockGroup.entityIds.Store(entityId, nil)
+
+			ch := GetChannel(common.ChannelId(entityId))
+			if ch == nil {
+				continue
+			}
+
+			if ch.entityController == nil {
+				ch.Logger().Error("channel doesn't have the entity controller")
+				continue
+			}
+
+			// All entity channels of the same group share the same lock group instance
+			ch.entityController.cascadeGroup(t, ctl.lockGroup)
 		}
 	}
 
 	return nil
 }
 
-func (ctl *EntityChannelGroupController) RemoveFromGroup(t channeldpb.EntityGroupType, entitiesToRemove []EntityId) error {
+func (ctl *FlatEntityGroupController) RemoveFromGroup(t channeldpb.EntityGroupType, entitiesToRemove []EntityId) error {
+	if t == channeldpb.EntityGroupType_HANDOVER {
+		if ctl.handoverGroup != nil {
+			for _, entityId := range entitiesToRemove {
+				ctl.handoverGroup.entityIds.Delete(entityId)
+				// Reset the removed entity's entity channel's handover group
+				entityCh := GetChannel(common.ChannelId(entityId))
+				if entityCh != nil {
+					entityCh.entityController.(*FlatEntityGroupController).handoverGroup = newEntityGroup()
+				}
+			}
+		} else {
+			return fmt.Errorf("handover group is nil, entityId: %d", ctl.entityId)
+		}
+	} else if t == channeldpb.EntityGroupType_LOCK {
+		if ctl.lockGroup != nil {
+			for _, entityId := range entitiesToRemove {
+				ctl.lockGroup.entityIds.Delete(entityId)
+				// Reset the removed entity's entity channel's lock group
+				entityCh := GetChannel(common.ChannelId(entityId))
+				if entityCh != nil {
+					entityCh.entityController.(*FlatEntityGroupController).lockGroup = newEntityGroup()
+				}
+			}
+		} else {
+			return fmt.Errorf("lock group is nil, entityId: %d", ctl.entityId)
+		}
+	}
+
 	return nil
 }
 
-func (ctl *EntityChannelGroupController) GetHandoverEntities() []EntityId {
+func (ctl *FlatEntityGroupController) GetHandoverEntities() []EntityId {
+	// If AddToGroup is never called, return the entity itself
 	if ctl.handoverGroup == nil {
-		return make([]EntityId, 0)
+		return []EntityId{ctl.entityId}
 	}
 
 	ctl.rwLock.RLock()
 	defer ctl.rwLock.RUnlock()
 
 	arr := make([]EntityId, 0, ctl.handoverGroup.entityIds.Size())
+	locked := false
 	ctl.handoverGroup.entityIds.Range(func(key EntityId, _ interface{}) bool {
+		if ctl.lockGroup != nil {
+			// If any entity in the handover group is locked, the handover should not happen.
+			if _, locked = ctl.lockGroup.entityIds.Load(key); locked {
+				return false
+			}
+		}
 		arr = append(arr, key)
 		return true
 	})
+
+	if locked {
+		return []EntityId{}
+	}
+
 	return arr
 }
 
@@ -125,9 +215,23 @@ func handleAddEntityGroup(ctx MessageContext) {
 		return
 	}
 
-	if addMsg.JunctionEntityId != ctx.ChannelId {
-		ctx.Connection.Logger().Error("AddEntityGroupMessage should be handled in the junction entity channel",
-			zap.Uint32("junctionEntityId", addMsg.JunctionEntityId), zap.Uint32("channelId", ctx.ChannelId))
+	if ctx.Channel.entityController == nil {
+		ctx.Channel.Logger().Error("channel doesn't have the entity controller")
+		return
+	}
+
+	if ctx.Channel.entityController.AddToGroup(addMsg.Type, CopyArray[uint32, EntityId](addMsg.EntitiesToAdd)) != nil {
+		ctx.Channel.Logger().Error("failed to add entities to group",
+			zap.Int32("groupType", int32(addMsg.Type)),
+			zap.Uint32s("entitiesToAdd", addMsg.EntitiesToAdd),
+		)
+	}
+}
+
+func handleRemoveEntityGroup(ctx MessageContext) {
+	removeMsg, ok := ctx.Msg.(*channeldpb.RemoveEntityGroupMessage)
+	if !ok {
+		ctx.Connection.Logger().Error("message is not an RemoveEntityGroupMessage, will not be handled.")
 		return
 	}
 
@@ -136,15 +240,10 @@ func handleAddEntityGroup(ctx MessageContext) {
 		return
 	}
 
-	if ctx.Channel.entityController.AddToGroup(addMsg.Type, EntityId(addMsg.JunctionEntityId), CopyArray[uint32, EntityId](addMsg.EntitiesToAdd)) != nil {
-		ctx.Channel.Logger().Error("failed to add entities to group",
-			zap.Int32("groupType", int32(addMsg.Type)),
-			zap.Uint32("junctionEntityId", addMsg.JunctionEntityId),
-			zap.Uint32s("entitiesToAdd", addMsg.EntitiesToAdd),
+	if ctx.Channel.entityController.RemoveFromGroup(removeMsg.Type, CopyArray[uint32, EntityId](removeMsg.EntitiesToRemove)) != nil {
+		ctx.Channel.Logger().Error("failed to remove entities from group",
+			zap.Int32("groupType", int32(removeMsg.Type)),
+			zap.Uint32s("entitiesToRemove", removeMsg.EntitiesToRemove),
 		)
 	}
-}
-
-func handleRemoveEntityGroup(ctx MessageContext) {
-
 }
