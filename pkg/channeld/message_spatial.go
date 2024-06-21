90aa18b8690bd40a74d0f0b93a2dd0bbb66bd409
indiest <indiestian@gmail.com>
2024-06-21 13:13:04 +0800

    - Suppressed error when using entity channel without SpatialController;

--- pkg/channeld/message_spatial.go
+++ pkg/channeld/message_spatial.go
@@ -228,7 +228,7 @@ func handleCreateEntityChannel(ctx MessageContext) {
 
 			// If the entity channel is created from GLOBAL channel(master server), but its data contains spatial info,
 			// we should set the owner of the entity channel to the spatial channel's.
-			if ctx.Channel == globalChannel {
+			if ctx.Channel == globalChannel && spatialController != nil {
 				dataMsgWithSpatialInfo, ok := dataMsg.(EntityChannelDataWithSpatialInfo)
 				if ok {
 					spatialInfo := dataMsgWithSpatialInfo.GetSpatialInfo()
